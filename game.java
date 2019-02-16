4b726d96a08cc6b720a3e21b2c84bfd226829b1e
nevinhall <46722804+nevinhall@users.noreply.github.com>
2019-02-16 16:57:39 +0000

    Add files via upload
    now compares lotto numbers with player numbers and returns have won the lotto if all numbers correct, you have gotten 4 numbers correct or you have not won the lotto if less then four numbers are correct.

--- game.java
+++ game.java
@@ -6,43 +6,51 @@ public class game {
 	private int  numLottoNumbersAllowed = 5;
 	private  ArrayList<Integer> lottoNumbers = new ArrayList<>();
 	private  ArrayList<Integer> playerNumbers = new ArrayList<>();
-	
-	
+
+
 	public game() {
-		
-		 
-	
-	    
-		
+
+
+
+
+
 	}
-	
+
 	public void  generateLottoNumbers() {
-		
+
 		for(int i = 0; i < numLottoNumbersAllowed; i++ ) {
 			int res = (int)(Math.random() * 50 + 1);
 			lottoNumbers.add(res);
-			System.out.println("Lotto Number " + i +  " is" + lottoNumbers.get(i));
-		
-			
-			
+//			System.out.println("Lotto Number " + i +  " is" + lottoNumbers.get(i));
+
 		}
-		
-	
-		
-		
 	}
-	
+
+
 	
 	public void playerNumbers( ArrayList<Integer> playerNumbers) {
+		int MinToWin = 0;
 		if(playerNumbers.equals(lottoNumbers)) {
-			System.out.println("Congratulations you have on the lotto");
+			MinToWin = 5;
+			System.out.println("Congratulations you have won the lotto");
+		}else
+			for(int i = 0; i < lottoNumbers.size(); i++) {
+//				System.out.println( playerNumbers.get(i));			
+				if( lottoNumbers.get(i) == playerNumbers.get(i)) {
+//					System.out.println("player numbers:"+ playerNumbers.get(i));
+					MinToWin ++;
+				}			
+			}
+		if( MinToWin == 4){
+			System.out.println("you've gotten four lotto numbers correct");
+		}else if(MinToWin > 4) {
 		}
-		
-		
-		
+		else
+			System.out.println("you havent won sorry ");
 	}
-		
 
+
+	
 	public int getNumLottoNumbersAllowed() {
 		return numLottoNumbersAllowed;
 	}
@@ -59,9 +67,17 @@ public class game {
 		this.lottoNumbers = lottoNumbers;
 	}
 
-
-	
+	public ArrayList<Integer> getPlayerNumbers() {
+		return playerNumbers;
 	}
+
+	public void setPlayerNumbers(ArrayList<Integer> playerNumbers) {
+		this.playerNumbers = playerNumbers;
+	}
+
+
+
+}
 
 
 
